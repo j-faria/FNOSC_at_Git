db33d104fd73593daf552880c7958584e7dadd7c
João Faria <Joao.Faria@astro.up.pt>
2012-08-30 22:25:01 +0100

    Added folder with original ZAMS SE code from H&K and in/out files

---
+++ zams_code/README_zams.for
@@ -0,0 +1,7 @@
+This is not the original zams code distributed by Hansen and Kawaler. Anil has edited the code so that every value may be input on one line.
+
+The revised version of ZAMS expects all the inputs on one line, as follows:
+Solar masses | X (Hyd abundance) | Y (He abundance) | Central pressure | Central Temperature | Radius | Solar Lumiosities | output file | want pulsation? (y/n)
+
+Note: Write scientific notation as "1.422e7".
+Table C.1 in Appendix C on page 430 of Hansen and Kawaler shows some initial guesses that converge for some masses of stars.
